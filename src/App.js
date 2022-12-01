33e9f64456caa5c7b44ea43d972a2cd4830f5f87
Joanna Gołofit <jgolofit@future-processing.com>
2022-12-01 13:42:45 +0100

    256. Using useEffect with Redux

--- src/App.js
+++ src/App.js
@@ -2,13 +2,29 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import { useSelector } from 'react-redux';
+import { useEffect } from 'react';
 
 
 function App() {
   const cartVisible = useSelector(state => state.ui.cartIsVisible);
+  const cart = useSelector(state => state.cart);
   const state = useSelector(state => state);
 
-console.log('cały state', state)
+  console.log('cały state tak wyglada', state)
+
+  useEffect(() => {
+    fetch('https://http-5fa63-default-rtdb.europe-west1.firebasedatabase.app/cart.json', {
+      method: "PUT",
+      body: JSON.stringify(cart)
+    })
+    // first
+  
+    // return () => {
+    //   second
+    // }
+  }, [cart])
+  
+
   return (
     <Layout>
       {cartVisible && <Cart />}
--- src/components/Shop/ProductItem.js
+++ src/components/Shop/ProductItem.js
@@ -1,16 +1,20 @@
 import { useDispatch } from 'react-redux';
+
+import { cartActions } from '../../store/cart-slice';
 import Card from '../UI/Card';
 import classes from './ProductItem.module.css';
-import { cartActions } from '../../store/cart-slice';
-
 
 const ProductItem = (props) => {
-  const { id, title, price, description } = props;
   const dispatch = useDispatch();
+  
+  const { id, title, price, description } = props;
 
   const addToCartHandler = () => {
-    dispatch(cartActions.addItemToCart({ id, title, price }))
-
+    dispatch(cartActions.addItemToCart({
+      id,
+      title,
+      price
+    }))
   }
 
   return (
