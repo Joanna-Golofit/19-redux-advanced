62bf542aae5bfc7f45be0c5dd88a1d30f2b56865
Joanna Gołofit <jgolofit@future-processing.com>
2022-12-06 13:40:16 +0100

    moving sendCartData to different file - the one with actions only

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
 import Notification from './components/UI/Notification';
-import { sendCartData } from './store/cart-slice';
+import { sendCartData } from './store/cart-actions';
 import { useSelector, useDispatch } from 'react-redux';
 import { useEffect } from 'react';
 // import { uiActions } from './store/ui-slice';
